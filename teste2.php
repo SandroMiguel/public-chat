4a0c8231ebcd9e502b4a88304be38a33343fe223
Sandro Miguel Marques <sandromiguel@produlogia.com>
2016-08-01 10:05:02 +0100

    Testing
    Testing files

---
+++ teste2.php
@@ -0,0 +1,24 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors','On');
+
+require 'constants.inc.php';
+require 'DbConnPDO.class.php';
+require 'Chat.class.php';
+
+# 1. Ler as mensagens da base de dados
+try {
+    $chat = new Chat();
+    $messages = $chat->getMessages(10);
+    echo json_encode($messages);
+} catch (Exception $e) {
+    $message = 'Ocorreu um erro.';
+    echo json_encode(
+        array(
+            "action"        =>  "insert",
+            "notification"  =>  "error",
+            "message"       =>  $message
+        )
+    );
+    exit;
+}
